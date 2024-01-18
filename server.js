b7c3a52ddf50f86acd6efffb1bf2b8bd68e0c709
LAXMAND2509 <laxmandilliraj@gmail.com>
2024-01-18 12:27:24 +0530

    mern fyp

---
+++ server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+const path = require("path");
+
+dotenv.config();
+if (process.env.NODE_ENV == "local") {
+	app.use(
+		cors({
+			origin: "http://localhost:3000",
+			credentials: true,
+		})
+	);
+} else {
+	app.use(
+		cors({
+			credentials: true,
+		})
+	);
+}
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "./frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(
+			path.resolve(__dirname, "./", "frontend", "dist", "index.html")
+		);
+	});
+}
+
+const dbConnect = async () => {
+	try {
+		if (process.env.NODE_ENV == "local") {
+			await mongoose.connect(process.env.LOCAL_DB_URL);
+			console.log("Local database is connected...");
+		} else {
+			await mongoose.connect(process.env.MONGODB_URL);
+			console.log("production database is connected...");
+		}
+	} catch (error) {
+		console.log("database connection failed...");
+	}
+};
+dbConnect();
+const PORT = process.env.PORT;
+app.listen(PORT, () => console.log(`runing ${PORT}`));
